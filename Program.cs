dbde137621aab43e5bafee47459dd8f97a3e6384
eminalpernl <eminalpernl@gmail.com>
2024-04-12 13:16:56 +0200

    hello world with C#

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+string firstFriend = "Sado";
+string secondFriend = "Mustafa";
+
+Console.WriteLine($"My first friend is {firstFriend} second one is {secondFriend}");
